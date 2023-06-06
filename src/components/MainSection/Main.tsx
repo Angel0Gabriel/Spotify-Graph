be084990c0d9c665ebb6f80d8ab9eae62795f1fd
Ângelo Gabriel <angelobielmoreira@hotmail.com>
2023-06-06 12:08:39 -0300

    feat: input to increment the sliceLength

--- src/components/MainSection/Main.tsx
+++ src/components/MainSection/Main.tsx
@@ -2,7 +2,7 @@
 'use client'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import Playlist, { SongProps } from '../Music'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { use, useEffect, useMemo, useState } from 'react'
 import axios from 'axios'
 import { Edge, IGraph, MyGraph, Node } from '../Graph'
 
@@ -18,9 +18,11 @@ interface RevelanceSong extends SongProps {
 }
 
 export default function MainSection() {
+  const [absoluteTracks, setAbsoluteTracks] = useState<SongProps[]>([])
   const [allTracks, setAllTracks] = useState<SongProps[]>([])
   const [playlist, setPlaylist] = useState<SongProps[]>([])
   const [similars, setSimilars] = useState<SongProps[]>([])
+  const [sliceLength, setSliceLength] = useState<number>(15)
 
   const genres = ['rap', 'trap', 'pop', 'rock']
 
@@ -30,7 +32,7 @@ export default function MainSection() {
   async function getTracks(accessToken: string) {
     try {
       const data = await axios.get(
-        'https://api.spotify.com/v1/playlists/3IsxzDS04BvejFJcQ0iVyW',
+        'https://api.spotify.com/v1/playlists/37i9dQZEVXbMXbN3EUUhlg',
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -50,8 +52,7 @@ export default function MainSection() {
           src: song.track.album.images[0].url ?? '',
         }
       })
-      setSimilars(newTracks.slice(0, 20))
-      setAllTracks(newTracks.slice(0, 20))
+      setAbsoluteTracks(newTracks)
     } catch (error) {
       console.log(error)
     }
@@ -106,14 +107,14 @@ export default function MainSection() {
     const edges = generateEdges(allTracks)
     partialGraph.edges.push(...edges)
     return partialGraph
-  }, [allTracks])
+  }, [allTracks, sliceLength])
 
   const getPlaylistGraph = useMemo((): IGraph => {
     const partialGraph = generateNodes(playlist)
     const edges = generateEdges(playlist)
     partialGraph.edges.push(...edges)
     return partialGraph
-  }, [playlist])
+  }, [playlist, sliceLength])
 
   const mapSimilarsRelevance = () => {
     return similars
@@ -168,6 +169,15 @@ export default function MainSection() {
       .then((data) => getTracks(data.access_token))
   }, [])
 
+  useEffect(() => {
+    setSimilars(
+      absoluteTracks
+        .slice(0, sliceLength)
+        .filter((song) => !playlist.find((ps) => ps.id === song.id)),
+    )
+    setAllTracks(absoluteTracks.slice(0, sliceLength))
+  }, [absoluteTracks, sliceLength])
+
   return (
     <main className="flex-1 p-6 max-h-[89.5vh] overflow-y-auto">
       <div className="flex items-center gap-4">
@@ -180,6 +190,45 @@ export default function MainSection() {
         </button>
       </div>
 
+      <div className="flex flex-row w-full mt-4 align-middle justify-center gap-6">
+        <div className="flex justify-center items-center">
+          <label htmlFor="">Adicione o tamanho da lista de 1 a 50: </label>
+          <input
+            className="bg-transparent rounded-sm ml-3"
+            onChange={(event) => setSliceLength(Number(event.target.value))}
+            type="number"
+            value={sliceLength}
+            min={1}
+            max={50}
+          />
+        </div>
+
+        <a className="text-sm">
+          Rap is{' '}
+          <a className="text-lg font-bold" style={{ color: 'red' }}>
+            red
+          </a>
+        </a>
+        <a className="text-sm">
+          Trap is{' '}
+          <a className="text-lg font-bold" style={{ color: 'blue' }}>
+            blue
+          </a>
+        </a>
+        <a className="text-sm">
+          Pop is{' '}
+          <a className="text-lg font-bold" style={{ color: 'green' }}>
+            green
+          </a>
+        </a>
+        <a className="text-sm">
+          Rock is{' '}
+          <a className="text-lg font-bold" style={{ color: 'gray' }}>
+            black
+          </a>
+        </a>
+      </div>
+
       <div className="flex flex-row w-full h-1/2 mt-4">
         <div className="flex-1 border border-zinc-400">
           <MyGraph graph={getPlaylistGraph} />
@@ -188,20 +237,7 @@ export default function MainSection() {
           <MyGraph graph={getSimilarsGraph} />
         </div>
       </div>
-      <div className="flex flex-row w-full mt-4 align-middle justify-center gap-6">
-        <a>
-          Rap is <a style={{ color: 'red' }}>red</a>
-        </a>
-        <a>
-          Trap is <a style={{ color: 'blue' }}>blue</a>
-        </a>
-        <a>
-          Pop is <a style={{ color: 'green' }}>green</a>
-        </a>
-        <a>
-          Rock is <a style={{ color: 'gray' }}>black</a>
-        </a>
-      </div>
+
       <div className="flex justify-around flex-1 w-full">
         <div className="flex flex-col flex-1 w-1/2 mr-12">
           <h1 className="font-semibold text-3xl mt-10">Playlist</h1>
